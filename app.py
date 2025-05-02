a8a362ebef4168016aaad98aa776fe634c5564ec
Bryn Noel Ubald <55503826+bnubald@users.noreply.github.com>
2025-05-02 16:17:33 +0100

    refactor: Add more permissive status codes, prevent lots of out of bounds errors

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from rio_tiler.io import Reader, STACReader
 
+from titiler.core.errors import DEFAULT_STATUS_CODES, add_exception_handlers
 from titiler.core.factory import (
     MultiBaseTilerFactory,
     TilerFactory,
@@ -12,8 +13,13 @@ from titiler.extensions import (
     stacExtension,
     stacViewerExtension,
 )
+from titiler.mosaic.errors import MOSAIC_STATUS_CODES
 
 app = FastAPI()
+
+# Add exception handlers - Won't show lots of missingtiles warnings when outside of bounds.
+add_exception_handlers(app, DEFAULT_STATUS_CODES)
+add_exception_handlers(app, MOSAIC_STATUS_CODES)
 
 # Create a tiler for COGs
 cog_tiler = TilerFactory(
@@ -25,9 +31,7 @@ cog_tiler = TilerFactory(
     ],
 )
 
-# Add the tiler to the main FastAPI app
-app.include_router(cog_tiler.router, prefix="/cog", tags=["Cloud Optimized GeoTIFF"])
-
+# Not currently used for IceNet dashboard, but, would be nice to dig into this at some point.
 stac_tiler = MultiBaseTilerFactory(
     reader=STACReader,
     router_prefix="/stac",
@@ -37,15 +41,22 @@ stac_tiler = MultiBaseTilerFactory(
     ],
 )
 
+# Add titiler routers to the main FastAPI app
+app.include_router(cog_tiler.router, prefix="/cog", tags=["Cloud Optimised GeoTIFF"])
+
 app.include_router(
     stac_tiler.router, prefix="/stac", tags=["SpatioTemporal Asset Catalog"]
 )
 
+
+# Don't find the non-existent main root to be useful, point to Swagger docs instead
 @app.get("/", response_class=RedirectResponse)
 async def redirect_to_docs():
     return "/docs"
 
+
 from rio_tiler.colormap import ColorMaps
+
 
 @app.get("/utils/colormaps")
 async def colormaps():
